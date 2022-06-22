40986f3b7ffdd1fafcd380fed74e167819e63d0e
chrystianobezerra <97260350+chrystianobezerra@users.noreply.github.com>
2022-06-22 16:51:15 -0300

    Correção 4

--- providers.tf
+++ providers.tf
@@ -2,17 +2,14 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "4.24.0"
+      version = "4.22.0"
     }
   }
 }
 
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
-
-  credentials = file("/home/vagrant/gcpkey.json")
-
-  project = "lab-devops-cloud-354023"
-  region  = "us-west1"
-  zone    = "us-west1-b"
-}
+    credentials = file("/home/vagrant/gcpkey.json")
+    project = "lab-devops-cloud-354023"
+    region  = "us-west1"
+    zone    = "us-west1-b"}
